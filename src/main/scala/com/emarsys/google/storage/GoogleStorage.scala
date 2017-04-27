096d86423ed934f7b7856321c45f4878c7b60462
andrasp3a <andras.papp@emarsys.com>
2017-04-27 14:59:54 +0200

    GoogleStorage trait and object

--- src/main/scala/com/emarsys/google/storage/GoogleStorage.scala
+++ src/main/scala/com/emarsys/google/storage/GoogleStorage.scala
@@ -1,22 +1,14 @@
 package com.emarsys.google.storage
 
-import java.io.ByteArrayInputStream
-
 import akka.actor.ActorSystem
-import akka.event.Logging
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
-import com.google.api.services.storage.StorageScopes
-import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.ReadChannel
-import com.google.cloud.storage.{Bucket, Storage, StorageOptions}
-
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success, Try}
 
 
-object GoogleStorage {
-
+trait GoogleStorage {
 
   def storageSource(fileName: String, chunkSize: Int = 64)(implicit ec: ExecutionContext, actorSystem: ActorSystem) : Source[ByteString, _] =  {
     val config = DefaultConfig(actorSystem)
@@ -30,5 +22,6 @@ object GoogleStorage {
 
   private def createChannel(project: String, bucket: String, fileName: String)(implicit actorSystem: ActorSystem) : Try[ReadChannel] = Try( GoogleStorageService(project).reader(bucket, fileName))
 
-
 }
+
+object GoogleStorage extends GoogleStorage
